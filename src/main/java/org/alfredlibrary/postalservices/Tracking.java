9650fa8aaccaba9a69f4277fc41f5beba86d873f
Alfred Library <library.alfred@gmail.com>
2011-10-03 14:57:23 -0300

    importing first implementations.

--- src/main/java/org/alfredlibrary/postalservices/Tracking.java
+++ src/main/java/org/alfredlibrary/postalservices/Tracking.java
@@ -1,11 +1,22 @@
 package org.alfredlibrary.postalservices;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Defines the common behavior to get tracking informations.
  * 
  * @author Marlon Silva Carvalho
  * @since 2.0.0
  */
-public interface Tracking {
+public interface Tracking extends Serializable {
+
+	/**
+	 * Get tracking statuses.
+	 * 
+	 * @param code Tracking code.
+	 * @return List of tracking statuses.
+	 */
+	List<Status> track(String code);
 
 }
