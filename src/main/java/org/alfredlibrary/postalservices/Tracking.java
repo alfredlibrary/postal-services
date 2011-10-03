12d7f20374b2bd69855fa0275a7accfea6145a46
Alfred Library <library.alfred@gmail.com>
2011-10-03 14:22:50 -0300

    adding first code! :)

---
+++ src/main/java/org/alfredlibrary/postalservices/Tracking.java
@@ -0,0 +1,11 @@
+package org.alfredlibrary.postalservices;
+
+/**
+ * Defines the common behavior to get tracking informations.
+ * 
+ * @author Marlon Silva Carvalho
+ * @since 2.0.0
+ */
+public interface Tracking {
+
+}
