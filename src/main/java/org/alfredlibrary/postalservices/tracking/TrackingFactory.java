5b131f7a20a610f6b90392cc8033ceffc332f958
Alfred Library <library.alfred@gmail.com>
2011-10-07 10:45:07 -0300

    preparing to CDI and creating a factory.

---
+++ src/main/java/org/alfredlibrary/postalservices/tracking/TrackingFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Alfred Library.
+ * Copyright (C) 2011 Alfred Team
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.alfredlibrary.postalservices.tracking;
+
+import org.alfredlibrary.postalservices.internal.tracking.CorreiosTracking;
+
+/**
+ * Create instances of {@link Tracking}. <br>
+ * If you don't use CDI, then use this factory to get instances of {@link Tracking}.<br>
+ * If you're using CDI, then use the @Inject and our qualifiers (@USPS, @Correios and so on).
+ * 
+ * @author Marlon Silva Carvalho
+ * @since 2.0.0
+ */
+final public class TrackingFactory {
+
+	public static Tracking getInstance(TrackingServices services) {
+		Tracking tracking = null;
+
+		if (services == TrackingServices.CORREIOS) {
+			tracking = new CorreiosTracking();
+		}
+
+		return tracking;
+	}
+
+}
