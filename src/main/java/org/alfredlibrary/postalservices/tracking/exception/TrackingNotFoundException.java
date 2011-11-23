c4867534da1a87c84ae0f0bfe2928513079ce513
Marlon Carvalho <marlon.carvalho@gmail.com>
2011-11-23 20:04:17 -0200

    changing exceptions package.

---
+++ src/main/java/org/alfredlibrary/postalservices/tracking/exception/TrackingNotFoundException.java
@@ -0,0 +1,46 @@
+/*
+ * Alfred Library.
+ * Copyright (C) 2011 Alfred Team
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.alfredlibrary.postalservices.tracking.exception;
+
+/**
+ * When no tracking status is found.
+ * 
+ * @author Marlon Silva Carvalho
+ * @since 2.0.0
+ */
+public class TrackingNotFoundException extends TrackingException {
+	private static final long serialVersionUID = 1L;
+
+	public TrackingNotFoundException() {
+		super();
+	}
+
+	public TrackingNotFoundException(String message, Throwable exception) {
+		super(message, exception);
+	}
+
+	public TrackingNotFoundException(String message) {
+		super(message);
+	}
+
+	public TrackingNotFoundException(Throwable exception) {
+		super(exception);
+	}
+
+}
